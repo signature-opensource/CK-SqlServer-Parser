9e72133ef71689e8f5ed9958641dc84e2a57376c
Olivier Spinelli <olivier.spinelli@signature-code.com>
2022-07-03 18:15:33 +0200

    Removed IsExplicitAllowed.

--- Tests/CK.SqlServer.UtilTests/ConsoleTests.cs
+++ Tests/CK.SqlServer.UtilTests/ConsoleTests.cs
@@ -10,7 +10,6 @@ namespace CK.SqlServer.UtilTests
         [Explicit]
         public void toggle_console()
         {
-            Assume.That( MonitorTestHelper.TestHelper.IsExplicitAllowed, "Press Ctrl key to allow this test to run." );
             MonitorTestHelper.TestHelper.LogToConsole = !MonitorTestHelper.TestHelper.LogToConsole;
         }
     }
